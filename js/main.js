edf489912e27680307216b864a8fd72424b84a7d
rameem2003 <rameem2019@gmail.com>
2022-12-28 14:50:09 +0600

    ui bug fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 const menu = document.querySelector("header .menu");
 const menuBtn = document.getElementById("tog");
 
-const bannerSection = document.querySelector(".banner")
-
-// console.log(bannerSection);
-const bannerPhotos = ["banner.jpg", "gallery0.jpg", "gallery1.jpg", "gallery2.jpg", "gallery3.jpg", "gallery4.jpg", "gallery5.jpg"];
-
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
 })
@@ -13,12 +8,4 @@ menuBtn.addEventListener("click", () => {
 
 window.onscroll = () => {
     menu.classList.remove("active");
-}
-
-window.onload = () => {
-    let randomNumber = Math.floor(Math.random() * 6) + 1
-    console.log(randomNumber);
-
-
-    bannerSection.style.backgroundImage = `url('./assets/img/our_gallery/${bannerPhotos[randomNumber]}')`
 }
