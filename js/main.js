a31655851d26e82a37a32003605344a05c2c82f1
rameem2003 <rameem2019@gmail.com>
2023-01-08 09:33:35 +0600

    update

--- js/main.js
+++ js/main.js
@@ -9,3 +9,10 @@ menuBtn.addEventListener("click", () => {
 window.onscroll = () => {
     menu.classList.remove("active");
 }
+
+
+let date = new Date();
+
+const lower_footer = document.querySelector(".lower-footer h4");
+
+lower_footer.innerHTML = `${date.getFullYear()} Team Republic of Legends. All Rights Resurved.`
