838c1bee2b7667ef6e310f535bc1ec0603fdd134
rameem2003 <rameem2019@gmail.com>
2023-01-31 21:42:09 +0600

    update

--- js/main.js
+++ js/main.js
@@ -13,6 +13,28 @@ window.onscroll = () => {
 
 let date = new Date();
 
-const lower_footer = document.querySelector(".lower-footer h4");
 
-lower_footer.innerHTML = `${date.getFullYear()} Team Republic of Legends. All Rights Resurved.`
+const footer = document.querySelector("footer");
+
+footer.innerHTML = `
+                    <div class="container">
+                        <div class="upper-footer">
+                            <h1>Republic of Legends</h1>
+                            <h3>Follow Us</h3>
+                            <div class="links">
+                                <ul>
+                                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                    <li><a href="https://www.youtube.com/@we.are.republicoflegends2022"><i class="fa-brands fa-youtube"></i></a></li>
+                                    <li><a href="https://github.com/rol2022"><i class="fa-brands fa-github"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <hr>
+
+                        <div class="lower-footer">
+                            <h4>${date.getFullYear()} Team Republic of Legends. All Rights Resurved.</h4>
+                        </div>
+                    </div>
+`;
