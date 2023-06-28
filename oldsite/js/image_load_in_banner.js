7094b0971d9995240bb2e5fdb0261825ce96a7b3
rameem2003 <rameem2019@gmail.com>
2023-06-28 21:44:54 +0600

    upload new technology react

---
+++ oldsite/js/image_load_in_banner.js
@@ -0,0 +1,20 @@
+const bannerSection = document.getElementById("banner")
+
+// console.log(bannerSection);
+const bannerPhotos = [
+  "banner.jpg",
+  "gallery0.jpg",
+  "gallery1.jpg",
+  "gallery2.jpg",
+  "gallery3.jpg",
+  "gallery4.jpg",
+  "gallery5.jpg",
+  "gallery6.jpg",
+];
+
+window.onload = () => {
+  let randomNumber = Math.floor(Math.random() * 7) + 1;
+  console.log(randomNumber);
+
+  bannerSection.style.backgroundImage = `url('./assets/img/our_gallery/${bannerPhotos[randomNumber]}')`;
+};
